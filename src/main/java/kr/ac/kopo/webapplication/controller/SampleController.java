549e361e4d7e6fa356654fb3ab37820055d74932
ohsanghy09 <ohsanghy09@naver.com>
2024-04-18 10:44:17 +0900

    20240418 bootstrap template 적용

--- src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
+++ src/main/java/kr/ac/kopo/webapplication/controller/SampleController.java
@@ -61,7 +61,7 @@ public class SampleController {
     public void ex3(){
 
     } // ex3}
-    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate"})
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate", "/exSidebar"})
     public void exLayout1(){
 
     } // exLayout1}
